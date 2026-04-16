85446f2d8b2446abdeb2e3addf7e470018087a9f
O <GAYATHRI.O@jcp.catalystbrands.com>
2026-04-16 10:43:15 +0530

    test

--- src/num.java
+++ src/num.java
@@ -53,7 +53,7 @@ public class num           //multiplication table
     public static void main(String[] args)
     {
         int n = 5;
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 10; i++)//loop
         {
             System.out.println(n + "x" + i + "=" + (n * i));
         }
